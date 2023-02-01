00929c6480bf55c11e1f5f177b2b7a0147d9bb7f
bhushansh26 <bhushanshelke30@gmail.com>
2023-02-01 18:38:47 +0530

    adding all files

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class Sum{
       public static void main (String args[]) {
             int a = 5;
             int b = 10;
-            System.out.println(a+b);
+            System.out.println("sum is"+(a+b));
       }
 
 }
